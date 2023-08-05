d15a5518061124363103906abefebb17f66b982b
gulkabakci <gkfrncsc@gmail.com>
2023-08-05 16:35:05 +0300

    start

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
        <Router>
-        {/*  */}
+       <Navbar/>
       <Routes>
         <Route path="/"  element={<Home/>} />
       </Routes>
