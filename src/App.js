0f7dbc92cad75a934e59a0e020ef425cbd168d06
gulkabakci <gkfrncsc@gmail.com>
2023-08-06 00:28:45 +0300

    dashboard updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
